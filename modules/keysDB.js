3e62fafc8ea8f1b433496f6f08690b2159c04b7f
Aliaksandr Andreichuk <coldlycalculatedsoul@gmail.com>
2023-04-28 13:59:42 +0300

    feat: add module keysDB

---
+++ modules/keysDB.js
@@ -0,0 +1,450 @@
+export const keys = [
+    {
+        name: 'Backquote',
+        value: '`',
+        shiftValue: '~',
+        rusValue: 'ё',
+        rusShiftValue: 'Ё'
+    },
+    {
+        name: 'Digit1',
+        value: '1',
+        shiftValue: '!',
+        rusValue: '1',
+        rusShiftValue: '!'
+    },
+    {
+        name: 'Digit2',
+        value: '2',
+        shiftValue: '@',
+        rusValue: '2',
+        rusShiftValue: '"'
+    },
+    {
+        name: 'Digit3',
+        value: '3',
+        shiftValue: '#',
+        rusValue: '3',
+        rusShiftValue: '№'
+    },
+    {
+        name: 'Digit4',
+        value: '4',
+        shiftValue: '$',
+        rusValue: '4',
+        rusShiftValue: ';'
+    },
+    {
+        name: 'Digit5',
+        value: '5',
+        shiftValue: '%',
+        rusValue: '5',
+        rusShiftValue: '%'
+    },
+    {
+        name: 'Digit6',
+        value: '6',
+        shiftValue: '^',
+        rusValue: '6',
+        rusShiftValue: ':'
+    },
+    {
+        name: 'Digit7',
+        value: '7',
+        shiftValue: '&',
+        rusValue: '7',
+        rusShiftValue: '?'
+    },
+    {
+        name: 'Digit8',
+        value: '8',
+        shiftValue: '*',
+        rusValue: '8',
+        rusShiftValue: '*'
+    },
+    {
+        name: 'Digit9',
+        value: '9',
+        shiftValue: '(',
+        rusValue: '9',
+        rusShiftValue: '('
+    },
+    {
+        name: 'Digit0',
+        value: '0',
+        shiftValue: ')',
+        rusValue: '0',
+        rusShiftValue: ')'
+    },
+    {
+        name: 'Minus',
+        value: '-',
+        shiftValue: '_',
+        rusValue: '-',
+        rusShiftValue: '_'
+    },
+    {
+        name: 'Equal',
+        value: '=',
+        shiftValue: '+',
+        rusValue: '=',
+        rusShiftValue: '+'
+    },
+    {
+        name: 'Backspace',
+        value: 'Backspace',
+        shiftValue: 'Backspace',
+        rusValue: 'Backspace',
+        rusShiftValue: 'Backspace'
+    },
+    {
+        name: 'Tab',
+        value: 'Tab',
+        shiftValue: 'Tab',
+        rusValue: 'Tab',
+        rusShiftValue: 'Tab'
+    },
+    {
+        name: 'KeyQ',
+        value: 'q',
+        shiftValue: 'Q',
+        rusValue: 'й',
+        rusShiftValue: 'Й'
+    },
+    {
+        name: 'KeyW',
+        value: 'w',
+        shiftValue: 'W',
+        rusValue: 'ц',
+        rusShiftValue: 'Ц'
+    },
+    {
+        name: 'KeyE',
+        value: 'e',
+        shiftValue: 'E',
+        rusValue: 'у',
+        rusShiftValue: 'У'
+    },
+    {
+        name: 'KeyR',
+        value: 'r',
+        shiftValue: 'R',
+        rusValue: 'к',
+        rusShiftValue: 'К'
+    },
+    {
+        name: 'KeyT',
+        value: 't',
+        shiftValue: 'T',
+        rusValue: 'е',
+        rusShiftValue: 'Е'
+    },
+    {
+        name: 'KeyY',
+        value: 'y',
+        shiftValue: 'Y',
+        rusValue: 'н',
+        rusShiftValue: 'Н'
+    },
+    {
+        name: 'KeyU',
+        value: 'u',
+        shiftValue: 'U',
+        rusValue: 'г',
+        rusShiftValue: 'Г'
+    },
+    {
+        name: 'KeyI',
+        value: 'i',
+        shiftValue: 'I',
+        rusValue: 'ш',
+        rusShiftValue: 'Ш'
+    },
+    {
+        name: 'KeyO',
+        value: 'o',
+        shiftValue: 'O',
+        rusValue: 'щ',
+        rusShiftValue: 'Щ'
+    },
+    {
+        name: 'KeyP',
+        value: 'p',
+        shiftValue: 'P',
+        rusValue: 'з',
+        rusShiftValue: 'З'
+    },
+    {
+        name: 'BracketLeft',
+        value: '[',
+        shiftValue: '{',
+        rusValue: 'х',
+        rusShiftValue: 'Х'
+    },
+    {
+        name: 'BracketRight',
+        value: ']',
+        shiftValue: '}',
+        rusValue: 'ъ',
+        rusShiftValue: 'Ъ'
+    },
+    {
+        name: 'Backslash',
+        value: '\\',
+        shiftValue: '|',
+        rusValue: '\\',
+        rusShiftValue: '/'
+    },
+    {
+        name: 'Delete',
+        value: 'Del',
+        shiftValue: 'Del',
+        rusValue: 'Del',
+        rusShiftValue: 'Del'
+    },
+    {
+        name: 'CapsLock',
+        value: 'CapsLock',
+        shiftValue: 'CapsLock',
+        rusValue: 'CapsLock',
+        rusShiftValue: 'CapsLock'
+    },
+    {
+        name: 'KeyA',
+        value: 'a',
+        shiftValue: 'A',
+        rusValue: 'ф',
+        rusShiftValue: 'Ф'
+    },
+    {
+        name: 'KeyS',
+        value: 's',
+        shiftValue: 'S',
+        rusValue: 'ы',
+        rusShiftValue: 'Ы'
+    },
+    {
+        name: 'KeyD',
+        value: 'd',
+        shiftValue: 'D',
+        rusValue: 'в',
+        rusShiftValue: 'В'
+    },
+    {
+        name: 'KeyF',
+        value: 'f',
+        shiftValue: 'F',
+        rusValue: 'а',
+        rusShiftValue: 'А'
+    },
+    {
+        name: 'KeyG',
+        value: 'g',
+        shiftValue: 'G',
+        rusValue: 'п',
+        rusShiftValue: 'П'
+    },
+    {
+        name: 'KeyH',
+        value: 'h',
+        shiftValue: 'H',
+        rusValue: 'р',
+        rusShiftValue: 'Р'
+    },
+    {
+        name: 'KeyJ',
+        value: 'j',
+        shiftValue: 'J',
+        rusValue: 'о',
+        rusShiftValue: 'О'
+    },
+    {
+        name: 'KeyK',
+        value: 'k',
+        shiftValue: 'K',
+        rusValue: 'л',
+        rusShiftValue: 'Л'
+    },
+    {
+        name: 'KeyL',
+        value: 'l',
+        shiftValue: 'L',
+        rusValue: 'д',
+        rusShiftValue: 'Д'
+    },
+    {
+        name: 'Semicolon',
+        value: ';',
+        shiftValue: ':',
+        rusValue: 'ж',
+        rusShiftValue: 'Ж'
+    },
+    {
+        name: 'Quote',
+        value: `'`,
+        shiftValue: '"',
+        rusValue: 'э',
+        rusShiftValue: 'Э'
+    },
+    {
+        name: 'Enter',
+        value: 'Enter',
+        shiftValue: 'Enter',
+        rusValue: 'Enter',
+        rusShiftValue: 'Enter'
+    },
+    {
+        name: 'ShiftLeft',
+        value: 'Shift',
+        shiftValue: 'Shift',
+        rusValue: 'Shift',
+        rusShiftValue: 'Shift'
+    },
+    {
+        name: 'KeyZ',
+        value: 'z',
+        shiftValue: 'Z',
+        rusValue: 'я',
+        rusShiftValue: 'Я'
+    },
+    {
+        name: 'KeyX',
+        value: 'x',
+        shiftValue: 'X',
+        rusValue: 'ч',
+        rusShiftValue: 'Ч'
+    },
+    {
+        name: 'KeyC',
+        value: 'c',
+        shiftValue: 'C',
+        rusValue: 'с',
+        rusShiftValue: 'С'
+    },
+    {
+        name: 'KeyV',
+        value: 'v',
+        shiftValue: 'V',
+        rusValue: 'м',
+        rusShiftValue: 'М'
+    },
+    {
+        name: 'KeyB',
+        value: 'b',
+        shiftValue: 'B',
+        rusValue: 'и',
+        rusShiftValue: 'И'
+    },
+    {
+        name: 'KeyN',
+        value: 'n',
+        shiftValue: 'N',
+        rusValue: 'т',
+        rusShiftValue: 'Т'
+    },
+    {
+        name: 'KeyM',
+        value: 'm',
+        shiftValue: 'M',
+        rusValue: 'ь',
+        rusShiftValue: 'Ь'
+    },
+    {
+        name: 'Comma',
+        value: ',',
+        shiftValue: '<',
+        rusValue: 'б',
+        rusShiftValue: 'Б'
+    },
+    {
+        name: 'Period',
+        value: '.',
+        shiftValue: '>',
+        rusValue: 'ю',
+        rusShiftValue: 'Ю'
+    },
+    {
+        name: 'Slash',
+        value: '/',
+        shiftValue: '?',
+        rusValue: '.',
+        rusShiftValue: ','
+    },
+    {
+        name: 'ArrowUp',
+        value: '▲',
+        shiftValue: '▲',
+        rusValue: '▲',
+        rusShiftValue: '▲'
+    },
+    {
+        name: 'ShiftRight',
+        value: 'Shift',
+        shiftValue: 'Shift',
+        rusValue: 'Shift',
+        rusShiftValue: 'Shift'
+    },
+    {
+        name: 'ControlLeft',
+        value: 'Сtrl',
+        shiftValue: 'Сtrl',
+        rusValue: 'Сtrl',
+        rusShiftValue: 'Сtrl'
+    },
+    {
+        name: 'MetaLeft',
+        value: 'Win',
+        shiftValue: 'Win',
+        rusValue: 'Win',
+        rusShiftValue: 'Win'
+    },
+    {
+        name: 'AltLeft',
+        value: 'Alt',
+        shiftValue: 'Alt',
+        rusValue: 'Alt',
+        rusShiftValue: 'Alt'
+    },
+    {
+        name: 'Space',
+        value: ' ',
+        shiftValue: ' ',
+        rusValue: ' ',
+        rusShiftValue: ' '
+    },
+    {
+        name: 'AltRight',
+        value: 'Alt',
+        shiftValue: 'Alt',
+        rusValue: 'Alt',
+        rusShiftValue: 'Alt'
+    },
+    {
+        name: 'ArrowLeft',
+        value: '◄',
+        shiftValue: '◄',
+        rusValue: '◄',
+        rusShiftValue: '◄'
+    },
+    {
+        name: 'ArrowDown',
+        value: '▼',
+        shiftValue: '▼',
+        rusValue: '▼',
+        rusShiftValue: '▼'
+    },
+    {
+        name: 'ArrowRight',
+        value: '►',
+        shiftValue: '►',
+        rusValue: '►',
+        rusShiftValue: '►'
+    },
+    {
+        name: 'ControlRight',
+        value: 'Ctrl',
+        shiftValue: 'Ctrl',
+        rusValue: 'Ctrl',
+        rusShiftValue: 'Ctrl'
+    },
+];
