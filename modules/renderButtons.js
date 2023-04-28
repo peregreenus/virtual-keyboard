c24315d2165dc7a36553e744d688dc9439e2cb10
Aliaksandr Andreichuk <coldlycalculatedsoul@gmail.com>
2023-04-28 15:11:59 +0300

    feat: add module renderButtons

---
+++ modules/renderButtons.js
@@ -0,0 +1,19 @@
+import { keys } from './keysDB.js';
+import { Button } from './button.js';
+
+export function renderButtons(target) {
+    const KEYS = [];
+
+    for (let i = 0; i < keys.length; i++) {
+        const keyBtn = new Button(keys[i].name, keys[i].value, keys[i].shiftValue, keys[i].rusValue, keys[i].rusShiftValue);
+        KEYS.push(keyBtn);
+    }
+
+    KEYS.slice(0, 14).forEach(item => item.addToRow(target[0]));
+    KEYS.slice(14, 29).forEach(item => item.addToRow(target[1]));
+    KEYS.slice(29, 42).forEach(item => item.addToRow(target[2]));
+    KEYS.slice(42, 55).forEach(item => item.addToRow(target[3]));
+    KEYS.slice(55).forEach(item => item.addToRow(target[4]));
+
+    return KEYS;
+}
--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 import { createPage } from './modules/createPage.js';
 import { keys } from './modules/keysDB.js';
 import { Button } from './modules/button.js';
+import { renderButtons } from './modules/renderButtons.js';
 
 const { header, textArea, keyboardWrapper } = createPage();
-// const rows = keyboardWrapper.querySelectorAll('.keyboard__row');
+const rows = keyboardWrapper.querySelectorAll('.keyboard__row');
+const KEYS = renderButtons(rows);
+
+console.log(KEYS);
